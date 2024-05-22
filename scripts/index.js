bd13405678de5ffa8eb7bbecbd49410c6bb166d6
ProphesierC <bwknowles98@gmail.com>
2024-05-21 22:18:47 -0400

    Associated with Task #7 Initialize terminal page with Class constructor

--- scripts/index.js
+++ scripts/index.js
@@ -1,61 +1,70 @@
 import { helpMe, enter, hackInit, hackFinish, helpTextFunction } from './helper.js';
 
-async function matrixNmap()
+class Terminal 
 {
-    await this.typing('enter', 100, 'sshnuke 10.2.2.2 -rootpw="********"');
-    for (let i = 21; i <= 48; i++)
+    constructor() 
     {
-        await new Promise(resolve => 
+        this.terminalOptions = 
         {
-            setTimeout(() => 
+            greetings: "[[!;;;;https://github.com/prophesierc]ProphesierC] v4.24 System Status: Online, Stable \nType 'help', '--h', or '-h' to ask for help. \n[[b;red;]ENTER] if you're ready",
+            prompt: '\nguest@prophesierC> ',
+            onCommandNotFound: () => this.terminal.echo(helpMe, { typing: true, delay: 0 })
+        };
+
+        this.commands = 
+        {
+            help: helpTextFunction,
+            HELP: helpTextFunction,
+            '--h': helpTextFunction,
+            '-h': helpTextFunction,
+            Enter: () => this.main(),
+            enter: () => this.main(),
+            ENTER: () => this.main()
+        };
+
+        this.terminal = $('body').terminal(this.commands, this.terminalOptions);
+        this.handleEnterKeyPress = this.handleEnterKeyPress.bind(this);
+        $('.terminal').on('keydown', this.handleEnterKeyPress);
+    }
+
+    async matrixNmap() 
+    {
+        await this.terminal.typing('enter', 100, 'sshnuke 10.2.2.2 -rootpw="********"');
+        for (let i = 21; i <= 48; i++) 
+        {
+            await new Promise(resolve => 
+            {
+                setTimeout(() => 
                 {
-                    this.echo(`Grid Node ${i} offline...`);
+                    this.terminal.echo(`Grid Node ${i} offline...`);
                     resolve();
                 }, 100);
+            });
         }
-    )};   
-}
-async function home()
-{
-    window.location.assign('root/home.html');
-}
-async function main()
-{
-    await this.echo(enter, { typing: true, delay: 0});
-    await matrixNmap.call(this);
-    await this.echo(hackInit, { typing: true, delay: 0});  
-    $('body').fadeOut('slow', 'swing')  
-    await this.echo(hackFinish, { typing: true, delay: 0});    
-    home();
-}
-
-const commands = 
-{
-    help: helpTextFunction,
-    HELP: helpTextFunction,
-    '--h': helpTextFunction,
-    '-h': helpTextFunction,
-    Enter: main,
-    enter: main,
-    ENTER: main   
-}
-
-const terminalOptions =
-{
-    greetings: "[[!;;;;https://github.com/prophesierc]ProphesierC] v4.24 System Status: Online, Stable \nType 'help', '--h', or '-h' to ask for help. \n[[b;red;]ENTER] if you're ready",
-    prompt: '\nguest@prophesierC> ',
-    onCommandNotFound: function() { this.echo(helpMe, { typing: true, delay: 0});}
-}
-
-
-
-let terminal = $('body').terminal(commands, terminalOptions);
-function handleEnterKeyPress(event) 
-{
-    if (event.keyCode === 13 && terminal.get_command().trim() === '') 
-    {
-        main.call(terminal);
-        $('.terminal').off('keydown', handleEnterKeyPress);
     }
+
+    async main()    
+    {
+        await this.terminal.echo(enter, { typing: true, delay: 0 });
+        await this.matrixNmap();
+        await this.terminal.echo(hackInit, { typing: true, delay: 0 });
+        $('body').fadeOut('slow', 'swing');
+        await this.terminal.echo(hackFinish, { typing: true, delay: 0 });
+        window.location.assign('root/home.html');
+    }
+
+    handleEnterKeyPress(event) 
+    {
+        if (event.keyCode === 13 && this.terminal.get_command().trim() === '') 
+        {
+            this.main();
+            $('.terminal').off('keydown', this.handleEnterKeyPress);
+        }
+    }
+    
 }
-$('.terminal').on('keydown',handleEnterKeyPress);
+
+$(document).ready(() => 
+{
+    const app = new Terminal();
+});
